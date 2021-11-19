0daf8e3c934e4901127e4cf37480353ca16ce100
AndrewsVieira <andrews.a.vieira@gmail.com>
2021-11-19 20:04:28 -0300

    Criand a tela de tarefa

--- webtodo/src/components/TaskManager/TaskManager.js
+++ webtodo/src/components/TaskManager/TaskManager.js
@@ -1,7 +1,38 @@
-import { React } from 'react';
+import { React, useEffect, useState } from 'react';
+import { Table } from 'reactstrap';
+import { Container } from 'react-bootstrap';
+import tasksRequest from '../../services/TaskService';
 
 export default function Tasks() {
+
+    const [tasks, setTasks] = useState([]);
+    useEffect(() => {
+        async function getTasks() {
+            setTasks(await tasksRequest());
+        }
+        getTasks();
+    }, []);
+
     return (
-       <>Task Manager</>
+        <Container>
+            <Table className="rankingPanel" xs="auto" >
+                <thead>
+                    <tr >
+                        <th scope="row" >Id</th>
+                        <th scope="row">Tarefa</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {tasks.map(res => {
+                        return (
+                            <tr>
+                                <td>{res.id}</td>
+                                <td>{res.task}</td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </Table>
+        </Container >
     );
 }
---
+++ webtodo/src/services/TaskService.js
@@ -0,0 +1,14 @@
+import Config from "../utils/Config";
+
+export default async function tasksRequest() {
+    return [
+        {
+            "id": 2,
+            "task": "ajustar o beckend"
+        },
+        {
+            "id": 3,
+            "task": "alterando a tarefa"
+        }
+    ]
+}
